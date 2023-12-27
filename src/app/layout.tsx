d9d5e7c555d59f00c835035241653cf5fa899c1e
i-am-robin <developerrobin@gmail.com>
2023-12-27 14:18:51 +0600

    added adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import GoogleAnalytics from "@bradgarropy/next-google-analytics";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,19 @@ export default function RootLayout({
     <html lang="en">
       <GoogleAnalytics measurementId="G-7CMM851EN0" />
 
+      {process.env.NODE_ENV === "production" && (
+        <Script
+          id="Adsense-id"
+          data-ad-client="ca-pub-4463310387942738"
+          async={true}
+          strategy="beforeInteractive"
+          onError={(e) => {
+            console.error("Script failed to load", e);
+          }}
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4463310387942738"
+        />
+      )}
+
       <body className={inter.className}>
         <Toaster richColors theme="dark" position="top-center" />
         {children}
@@ -32,3 +46,5 @@ export default function RootLayout({
 }
 
 // <meta name="google-site-verification" content="Sw4pHjtcNQ0-kAfHuHj9tP9-5_l5ePPLUE_w-qCgxrc" />
+// <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4463310387942738"
+// crossorigin="anonymous"></script>
